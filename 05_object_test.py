c4be0f3e0c13fcb3e97aeb47a2561cdb97ba0ff4
jinny <cdjung515sj@gmail.com>
2026-07-16 11:04:40 +0900

    제목 수정

---
+++ 05_object_test.py
@@ -0,0 +1,38 @@
+count = 27
+temp = 36.5
+name = "수진"
+is_ok = True
+print(count, temp, name, is_ok)
+
+print("count :", type(count))
+print("temp :", type(temp))
+print("name :", type(name))
+print("is_ok :", type(is_ok))
+
+print(type(100))  # int
+print(type(100.0))  # float
+print(type("100"))  # str
+# <class 'int'> / <class 'float'> / <class 'str'>
+
+print(15 + 5)
+print("15" + "5")
+# print("15" + 5) # TypeError
+# 더하기 : 숫자 - 연산 / 문자 - 이어쓰기 ==> 출력 : 1. 8 / 2. 155
+
+print(3 > 2)  # True # 출력 True
+print(5 == 5)  # True # 출력 True
+print(type(3 > 2))  # bool # 출력 <class 'bool'>
+
+count = 155
+print("count :", type(count))  # count : <class 'int'>
+count = 15.5
+print("count :", type(count))  # count : <class 'float'>
+count = "155"
+print("count :", type(count))  # count : <class 'str'>
+
+dvice_temp = 41.2  # <class 'float'>
+check_count = 7  #<class 'int'>
+device_name = "뷰티디바이서"  #<class 'str'>
+is_normal = True  # <class 'bool'>
+
+
