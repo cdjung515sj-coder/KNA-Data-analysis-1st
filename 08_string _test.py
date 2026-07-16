1e746ed5e28703c80dffce14dd8799ecec88c9a2
jinny <cdjung515sj@gmail.com>
2026-07-16 17:00:24 +0900

    인덱싱, 슬라이싱 실습

---
+++ 08_string _test.py
@@ -0,0 +1,24 @@
+test1 = "temp_sensor"
+print(test1[:4])
+
+test2 = "temp_sensor"
+print(test2[0:])
+
+test3 = "sensor_01"
+print(test3[-2:])
+
+test4 = "PYTHON"
+print(test4[::2])  # PTO
+print(test4[::-2])  # NHY 위와 반대로 출력됨⭐
+
+test5 = "PYTHON"
+print(test5[::-1])  # -1 뒤에서 앞으로 뛰기때문에 글자가 거꾸로 뒤집힘.
+
+
+test3 = "PYTHON"
+print(test3[0])
+print(test3[2])
+print(test3[5])
+
+test3 = "PYTHON"
+print(word[::-1])
