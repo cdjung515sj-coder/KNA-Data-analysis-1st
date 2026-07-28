fce18302bcaf398bbcbad57b52c0a7f4e1bb2851
jinny <cdjung515sj@gmail.com>
2026-07-28 11:37:48 +0900

    list 인덱스 값 꺼내고 계산하기

--- 2. practice/09_list_test.py
+++ 2. practice/09_list_test.py
@@ -4,3 +4,19 @@ print(today_temps)
 print(len(today_temps))
 empty = []
 print(len(empty))
+
+# 실습 2. 인덱스로 값 꺼내기
+list6 = [11,-2,44,13,5,-10]
+print(list6[0])
+print(list6[2])
+print(list6[-1])
+print(len(list6)) # 6
+print(list6[5]) # 6개니까 마지막 요소는 6-1 = 5
+
+# 실습 3. 인덱스로 꺼낸 값 계산하기
+list6 = [11,-2,44,13,5,-10]
+first = list6[0]
+last = list6[-1]
+sum = first+last
+print(sum)
+print(sum/2)
