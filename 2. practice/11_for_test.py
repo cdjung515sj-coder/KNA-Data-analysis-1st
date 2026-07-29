13d3e4c2391d90926e3ad062f553bb7e86aecfcd
jinny <cdjung515sj@gmail.com>
2026-07-29 16:05:23 +0900

    369 출력 게임 민호버전

--- 2. practice/11_for_test.py
+++ 2. practice/11_for_test.py
@@ -38,3 +38,31 @@ num = int(input("사용자 입력값 : "))
 for i in range(1, num + 1):
     if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
         print(i)
+
+
+print("\n=== 369 게임 출력 ===")
+bounds = str(input("입력: "))
+bounds_len = len(bounds)  # 몇자리수인지 확인하기 위함
+
+l = []  # 빈 리스트
+count_369 = 0
+for num in range(1, int(bounds) + 1):
+
+    str_num = str(num)
+    len_num = len(str_num)
+
+    for j in range(0, len(str_num)):
+        if (  # 각 인덱스 자리수 3, 6, 9 포함 확인
+            str_num[j] == "3" or str_num[j] == "6" or str_num[j] == "9"
+        ):
+            count_369 += 1
+
+    if count_369 > 0:
+        l.append("👏🏻" * count_369)
+    else:
+        l.append(num)
+
+    count_369 = 0
+
+
+print(f"369 결과: {l}")
