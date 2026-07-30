7cff88a5729c8e4b2018a950a1f4421b97370b37
jinny <cdjung515sj@gmail.com>
2026-07-30 09:09:20 +0900

    반짝반짝 작은 별 늘리기

---
+++ 2. practice/ex/for_ex.py
@@ -0,0 +1,37 @@
+# * 1
+# ** 2
+# *** 3
+# **** 4
+# ***** 5
+# ****** 6
+# ******* 7
+# ******** 8
+star = ""
+
+for i in range(1, 9):
+    for j in range(0, i):
+        star += "⭐"
+    star += "\n"
+print(star)
+
+
+# ==========================================
+
+star = ""
+
+for i in range(10, 0, -1):
+    for j in range(0, i):
+        star += "⭐"
+    star += "\n"
+print(star)
+
+# ==========================================
+
+star = ""
+
+for i in range(10, 0, -1):
+    if 
+        for j in range(0, i):
+        star += "⭐"
+    star += "\n"
+print(star)
