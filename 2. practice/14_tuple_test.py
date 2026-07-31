f535f94adf6775e9c59f079531069bc9c5dfef45
jinny <cdjung515sj@gmail.com>
2026-07-31 11:07:05 +0900

    실습 2 . 튜플 리스트 반복 처리

--- 2. practice/14_tuple_test.py
+++ 2. practice/14_tuple_test.py
@@ -10,3 +10,20 @@ print(sensor[1])
 
 name, value = sensor
 print(name, value)
+
+# 실습 2. 튜플 리스트를 반복 처리하기
+sensors = [
+    ("모터온도", 77),
+    ("모터진동", 10),
+    ("모터압력", 91),
+    ("회전속도", 1133),
+    ("유량", 42),
+]
+
+warning = 90
+for name, value in sensors:
+    print(name,value)
+
+for name, value in sensors:
+    if value > warning:
+        print(f"{name} warning!!")
