13973d371de7cd500505aa942110c5228bc2648c
jinny <cdjung515sj@gmail.com>
2026-08-04 16:04:06 +0900

    ⭐⭐⭐ 랜덤 추출 import random random.choice() return ddd.get(),ddd()

--- 1. lecture/17_function_2.py
+++ 1. lecture/17_function_2.py
@@ -181,3 +181,97 @@ avg = calc_average(75.3, 88.0)
 print(f"평균 온도: {avg}")
 
 
+print("000")
+
+
+# 여러 값을 한 번에 반환하기
+# 다음의 함수는 배열을 받아서 그 안의 최소값과 최대값을 동시에 return 한다.
+def calc_min_max(values):
+    minimum = min(values)  # 배열 안의 최소값을 찾아 minimum에 담기
+    maximum = max(values)  # 배열 안에 최대값을 찾아 maximum에 담기
+    return minimum, maximum
+
+
+target_list = [1, 2, 3, 4, 5, 6]
+result = calc_min_max(target_list)
+print(result)  # 튜플인 것들을 확인
+
+# 반환값을 언패킹으로 받기
+# 함수의 결과를 받는 순간에 결과 튜플의 내용을 풀어서 개별 변수에 담아 사용하기
+result_min, result_max = calc_min_max(target_list)
+print("최소값 : " + str(result_min))
+print("최대값 : " + str(result_max))
+
+# return 반환값이 없는 함수를 호출해놓고
+# 결과를 어디에 담겠냐고 하면, 담기는 값은 None이라고
+
+
+def say_greet():
+    print("만나서 반값습니다")
+    return
+
+
+greet = say_greet
+print(greet)  # None
+
+
+# ----------------------------------------
+# 지금까지 배우 내용을 활용해서
+# 재미있는 함수 만들기 예제
+
+import random
+
+groups = ["에스파", "하트2하트", "리센느", "태연", "엔믹스"]
+
+# 랜덤 뽑기!
+my_group = random.choice(groups)
+print(my_group)
+
+
+def get_random_group():
+    groups = [
+        {"이름": "에스파", "리더": "카리나"},
+        {"이름": "엔믹스", "리더": "해원"},
+        {"이름": "리센느", "리더": "원희"},
+    ]
+
+    my_group = random.choice(groups)
+
+    return my_group.get("이름"), my_group.get("리더")
+
+
+group_name, group_leader = get_random_group()
+print(f"{group_name}의 리더는 {group_leader}입니다")
+
+
+# ----------------------------
+# 3-4인과 함께 코드를 만드시오. 가봤거나, 가보고 싶은 여행지 정보를 모아보자 (최소 5개 이상)
+# 함수를 호출하면 랜덤으로 해당 여행지의 국가이름과 수도
+# "환영합니다! --- 나라의 수도 --- 입니다!" 출력
+
+
+import random
+
+travel_destinations = [
+    {"국가": "대한민국", "수도": "서울"},
+    {"국가": "일본", "수도": "도쿄"},
+    {"국가": "러시아", "수도": "모스크바"},
+    {"국가": "프랑스", "수도": "파리"},  # '유럽' 대신 프랑스로 설정
+    {"국가": "미국", "수도": "워싱턴 D.C."},
+]
+
+
+def recommend_travel():
+    selected_place = random.choice(travel_destinations)
+    country = selected_place["국가"]
+    capital = selected_place["수도"]
+    print(f"환영합니다! {country} 나라의 수도 {capital} 입니다!")
+
+
+recommend_travel()
+recommend_travel()
+recommend_travel()
+recommend_travel()
+recommend_travel()
+recommend_travel()
+recommend_travel()
