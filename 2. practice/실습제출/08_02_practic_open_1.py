b78aec10399e742ba6a1afed6eafb807c92fb9d9
jinny <cdjung515sj@gmail.com>
2026-08-06 11:37:06 +0900

    실습 1. open으로 파일 읽기

---
+++ 2. practice/실습제출/08_02_practic_open_1.py
@@ -0,0 +1,25 @@
+# 실습 1. open 으로 파일 읽기
+
+# read로 전체를 한 문자열로 출력
+f = open("data/sample.txt", "r", encoding="utf-8")
+d = open("data/sample.txt", "r", encoding="utf-8")
+
+
+read = f.read()
+lines = d.readlines()
+print(read)
+print("-----")
+print(lines)
+
+f.close()
+
+
+print(" ============================")
+
+# readlines로 줄 리스트로 읽어 출력하기
+with open("data/sample.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+print(lines)
+
+# 이건 with를 썻기 때문에 닫을 필요 없음
