b926d90cfb29b57791f6bb103a902ffca80f7ee7
jinny <cdjung515sj@gmail.com>
2026-08-06 10:44:18 +0900

    내장함수인 open()으로 파일을 읽어 f.read()로 파일 읽기 open()을 사용하면 f.close() 필수
    with ..... as f: 를 사용한다면 close 필요 없음

---
+++ 1. lecture/18_module_Standard_Library__read_3.py
@@ -0,0 +1,27 @@
+# 기본 내장함수인 open()으로 sample.txt 파일 열기
+# 읽기모드(r)로 utf-8 형식의 변환을 거쳐 읽기로 한다
+# 가져온 정도(파인 접근 열쇠/참조값)를 f에 담는다
+
+f = open("data/sample.txt", "r", encoding="utf-8")
+
+
+print(type(f).__name__)  # __name__ 이 언더바 두개씩은 약속임
+# 타입 이름 : TextIOWrapper
+
+# 텍스트파일 파일 한 줄씩 문자열 만들기
+lines = f.readlines()
+print(lines)
+
+f.close()  # 열었다면 꼭 닫아줘야함
+
+print("-=-" * 20)
+# 만약 신경써서 파일 닫기(close) 해주기 귀찮다면
+# with open ... as 문법을 쓰는 것도 좋다
+with open("data/sample.txt", "r", encoding="utf-8") as f:
+    # 앞으로 이렇게 들여쓰기 된 코드가 끝나면
+    # 파일 접은을 닫습니다(close)
+
+    # 텍스트파일 파일 한줄씩 문자열을 만들어 리스트만들기
+    lines = f.readlines()
+
+print(lines)
