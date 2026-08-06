38ba5983f09a474d993dffe396945ab84cdf6839
jinny <cdjung515sj@gmail.com>
2026-08-06 14:52:28 +0900

    row와 row.get 으로 직관적으로 정보를 볼 수 있음

--- 1. lecture/18_module_Standard_Library__read_3.py
+++ 1. lecture/18_module_Standard_Library__read_3.py
@@ -58,17 +58,17 @@ csv_path = os.path.join("data", "08_press.csv")
 
 # 위 경로의 파일을 찾지 못한다면 강제종료시키기
 if not os.path.exists(csv_path):
-    print("파일있음")
+    print("파일없음")
     sys.exit(1)  # 비정상 종료시 보통 0이 아닌 값(예 1) 전달
 
-print("파일없음")
+print("파일있음")
 
 with open(csv_path, "r", encoding="utf-8") as f:
     # print(f.readlines()) # 이제 csv 전문가에게 맡기자 !
     reader = csv.reader(f)
 
     for row in reader:
-        print(row)  # 각 행(row)마다 리스트로 출력됨
+        print(row[0])  # 각 행(row)마다 리스트로 출력됨
 
 # -------------------------
 
@@ -81,3 +81,29 @@ with open(csv_path, "w", encoding="utf-8", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(["시각", "설비"])
     writer.writerow(["09:00", "PUMP-01"])
+
+
+# --------------------------
+print("---------------------")
+
+import os
+import sys
+import csv
+
+csv_path = os.path.join("data", "08_press.csv")
+
+if not os.path.exists(csv_path):
+    print("파일없음")
+    sys.exit(1)  # 비정상 종료시 보통 0이 아닌 값(예 1) 전달
+
+print("파일있음")
+
+with open(csv_path, "r", encoding="utf-8") as f:
+    # DictReader는 첫줄은 컬럼 이름으로 판단하고
+    # 각 row를 ㅐ당 컬럼이름들을 key로 하는 딕셔너리로 만들어준다
+    reader = csv.DictReader(f)
+
+    print(reader)
+
+    for row in reader:
+        print(row["설비ID"], row.get("시각"))
