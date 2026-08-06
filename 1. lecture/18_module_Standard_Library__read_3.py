a03fd42f535c27a3c39f5136a1ca3da400434ebf
jinny <cdjung515sj@gmail.com>
2026-08-06 14:13:45 +0900

    csv 파일 읽고 쓰기

--- 1. lecture/18_module_Standard_Library__read_3.py
+++ 1. lecture/18_module_Standard_Library__read_3.py
@@ -46,3 +46,39 @@ f = open("data/hello.txt", "a", encoding="utf-8")
 f.write("맛점!!!\n")
 
 f.close()
+
+
+# ============================================================
+
+import os
+import sys
+import csv
+
+csv_path = os.path.join("data", "08_press.csv")
+
+# 위 경로의 파일을 찾지 못한다면 강제종료시키기
+if not os.path.exists(csv_path):
+    print("파일있음")
+    sys.exit(1)  # 비정상 종료시 보통 0이 아닌 값(예 1) 전달
+
+print("파일없음")
+
+with open(csv_path, "r", encoding="utf-8") as f:
+    # print(f.readlines()) # 이제 csv 전문가에게 맡기자 !
+    reader = csv.reader(f)
+
+    for row in reader:
+        print(row)  # 각 행(row)마다 리스트로 출력됨
+
+# -------------------------
+
+import os
+import sys
+import csv
+
+csv_path = os.path.join("data", "result.csv")
+
+with open(csv_path, "w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["시각", "설비"])
+    writer.writerow(["09:00", "PUMP-01"])
