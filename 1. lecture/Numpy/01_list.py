86d1ee902eb24c1587fc7e7376c8634f5ef9dee2
jinny <cdjung515sj@gmail.com>
2026-08-10 10:17:45 +0900

    numpy 리스트와 배열

---
+++ 1. lecture/Numpy/01_list.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+# -----------------------------
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers_10 = []
+
+for number in numbers:
+    # print(number)
+    numbers_10.append(number * 10)
+
+print(numbers_10)  # [10, 20, 30, 40, 50]
