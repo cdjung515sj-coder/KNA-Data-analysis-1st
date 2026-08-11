726e09b36361840267cb5fe11bdadf8b71fc8d78
jinny <cdjung515sj@gmail.com>
2026-08-11 17:31:25 +0900

    # 실습 5. 조건별 개수와 비율 세기

--- 2. practice/실습제출 Numpy/10_02_practice_5.py
+++ 2. practice/실습제출 Numpy/10_02_practice_5.py
@@ -22,4 +22,23 @@ print(round(high5.mean(), 1))  # 0.2, 소수점 이하 2자리까지 나오도�
 # 조건을 만족하는 값으 개수와 비율이 출력
 
 temp = np.array([11.123521, 325.151513, 133.2341])
-print(np.round(temp, 2)) # [ 11.12 325.15 133.23]
+print(np.round(temp, 2))  # [ 11.12 325.15 133.23]
+
+# ----------------------------------------------------------------------
+
+torque = np.array([42.8, 26.3, 49.4, 4.6, 41.9, 65.7, 12.5, 55.0])
+
+torque_over_40 = torque >= 40.0
+print(torque_over_40)
+
+over_sum = torque_over_40.sum()
+over_mean = torque_over_40.mean()
+
+print(over_sum)
+print(over_mean)
+
+
+print(f"전체 토크 데이터 : {torque}")
+print(f"조건 만족 여부   : {torque_over_40}개")
+print(f"40 이상인 데이터 개수 : {over_sum}개")
+print(np.round(torque, 2))
