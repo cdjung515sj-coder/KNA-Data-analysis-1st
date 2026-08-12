722850c09b4c933d7e132d3efb0491c1fc673e49
jinny <cdjung515sj@gmail.com>
2026-08-12 13:10:17 +0900

    pandas 설치는 가상환경에서 pip install pandas

---
+++ 1. lecture/Pandas/00_venv_setup.py
@@ -0,0 +1,16 @@
+# 1. 현재 경로에 가상환경 생성
+# 터미널에서 실행
+# python -m venv .venv
+
+
+# 2. 가상환경 활성화
+# source .venv/bin/activate (맥)
+# source .venv/Scripts/activate
+# (이후엔 가상환경 안에서 터미널 명령 실행 가능, 예) pip install numpy)
+
+
+# pandas는 가상환경 활성화 후 설치하면 됨
+# pip install pandas
+
+# 4. (작업/실행이 끝나고) 가상환경 종료
+# deactivate
