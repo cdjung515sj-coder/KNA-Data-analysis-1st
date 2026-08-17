93c625a7520fe0a0e86ff6f23e16315a4bf24622
jinny <cdjung515sj@gmail.com>
2026-08-17 22:05:39 +0900

    13_02_05_위험 순 정렬하고 상위 추출 .sort_values() .tolist() ascending=[True,False]

---
+++ 2. practice/Pandas_practice/13_02_05.py
@@ -0,0 +1,28 @@
+# 실습 5. 위험 순으로 정렬하기
+# 목표
+# 데이터를 위험한 순서로 정렬하고 상위만 추출
+
+import pandas as pd
+
+df_shot = pd.read_csv("data/13_diecasting_shot.csv")
+df_shot()
+
+# 단계
+df_sorted = df_shot.sort_values(by=["비스킷두께"])  # 기본 배열
+
+# · sort_values로 비스킷두께를 큰 값부터 내림차순 정렬
+df_sorted = df_shot.sort_values(by=["비스킷두께"], ascending=False)
+
+# · head로 상위 다섯 개만 추출해 값 확인
+print(df_sorted.head(5))
+
+# 직접 해당 값들만 뽑아서 list로 출력해보려면?
+print(df_sorted["비스킷두께"].head(5).tolist())
+
+
+# · 여러 열을 리스트로 묶어 우선순위 다중 정렬
+# 품질등급을 우선 오름차순으로 정렬하고 형체력을 그 다음 순서로 내림차순 정렬하기
+df_multisorted = df_shot.sort_values(["품질등급", "형체력"], ascending=[True, False])
+# 예상 결과
+# 상위 5개 비스킷두께 값과 다중 정렬 첫 행 품질등급 출력
+print(df_multisorted.head(5))
